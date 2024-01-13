b7f5bd94fc8cc4603c0a7207e964b2e0fb14cb44
FotimaTaxirovna <104978587+FotimaTaxirovna@users.noreply.github.com>
2024-01-13 19:19:02 +0500

    fgg
    fgd

--- nom1/src/pages/js/Main_persons.js
+++ nom1/src/pages/js/Main_persons.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import "../css/Main_persons.css";
 // import img10 from "../img/image 24.png"
-import img11 from "../img/image 25.png"
+// import img11 from "../img/image 25.png"
 import Navbar_page from "./Navbar_page";
 import { FaMoneyBillWave, FaPhone, FaStar } from "react-icons/fa";
 export default class Main_persons extends Component {
@@ -26,10 +26,191 @@ return (
           <button className="button4">ПОИСК</button>
         </div>
         <h2 className="h2">Результаты поиска (501)</h2>
-        <div className="glavn-div">
 
+        <div className="glavn-div">
+          <div className="main8">
+            <div className="main9">
+              <div className="main24">
+                <img
+                  className="img22"
+                  src="https://th.bing.com/th/id/OIP.3G6k73B4lvRM4SAno5JemgHaFz?rs=1&pid=ImgDetMainhttps://bilder.fernsehserien.de/gfx/person_1000/d/dave-franco.jpg"
+                  alt=""
+                />
+                <div className="main25">
+                  <FaStar className="star" />
+                  <FaStar className="star" />
+                  <FaStar className="star" />
+                  <FaStar className="star" />
+                  <FaStar className="star" />
+                  <p className="p1">0 отзовов</p>
+                </div>
+              </div>
+              <div className="main11">
+                <h4 className="h41">Yetta Rassmusen</h4>
+                <h4 className="h42">Ортопед. Психиатр</h4>
+                <h4 className="h43">Стаж 30 г.</h4>
+                <h4 className="h44">Высшая категория</h4>
+                <h4 className="h45">
+                  <FaMoneyBillWave className="icon-money" /> от 150000.00 сум
+                </h4>
+                <h4 className="h46">Запись на приём:</h4>
+                <h4 className="h47">
+                  <FaPhone className="icon-phone" /> +1 (809) 329-6178
+                </h4>
+                <h4 className="h48">lllum dolor quis ar</h4>
+              </div>
+            </div>
+
+            <div className="main9 ">
+              <div className="main24">
+                <img
+                  className="img23"
+                  src="https://img.freepik.com/free-photo/business-woman-working-in-office-on-computer_1303-28023.jpg?w=740&t=st=1670054008~exp=1670054608~hmac=3bff411bc801892e20d2c8ad0ac28d0e7b8d33973d0eee8ae3e55464f759f95f"
+                  alt=""
+                />
+                <div className="main25">
+                  <FaStar className="star" />
+                  <FaStar className="star" />
+                  <FaStar className="star" />
+                  <FaStar className="star" />
+                  <FaStar className="star" />
+                  <p className="p1">1 отзовов</p>
+                </div>
+              </div>
+              <div className="main12">
+                <h4 className="h41">Yetta Rassmusen</h4>
+                <h4 className="h42">Врач УЗИ</h4>
+                <h4 className="h43">Стаж 2 г.</h4>
+                <h4 className="h44">Высшая категория</h4>
+                <h4 className="h45">
+                  <FaMoneyBillWave className="icon-money" /> от 270000.00 сум
+                </h4>
+                <h4 className="h46">Запись на приём:</h4>
+                <h4 className="h47">
+                  <FaPhone className="icon-phone" /> +1 (805) 452-7194
+                </h4>
+                <h4 className="h48">Minima ut dolor non</h4>
+              </div>
+            </div>
+          </div>
+          <div className="kundd">
+            <div className="dix">
+              <div className="kalendar">
+                <div className="k1">
+                  <h1>Мединцинский центр "Бегма"</h1>
+                  <p className="we">(Cupldltate voluptate)</p>
+                  <div className="kun">
+                    <div className="kun1">
+                      <p>6</p>
+                      <p>cp</p>
+                    </div>
+                    <div className="kun2">
+                      <p>7</p>
+                      <p>чт</p>
+                    </div>
+                    <div className="kun3">
+                      <p>8</p>
+                      <p>пт</p>
+                    </div>
+                    <div className="kun4">
+                      <p>9</p>
+                      <p>сб</p>
+                    </div>
+                    <div className="kun5">
+                      <p>10</p>
+                      <p>вс</p>
+                    </div>
+                    <div className="kun6">
+                      <p>11</p>
+                      <p>пн</p>
+                    </div>
+                    <div className="kun7">
+                      <p>12</p>
+                      <p>вт</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="k2">
+                  <div className="soat1">
+                    <p className="padruga">16:00</p>
+                  </div>
+                  <div className="soat1">
+                    <p className="padruga">16:30</p>
+                  </div>
+                  <div className="soat1">
+                    <p className="padruga">17:00</p>
+                  </div>
+                  <div className="soat1">
+                    <p className="padruga">17:30</p>
+                  </div>
+                  <div className="soat1">
+                    <p className="padruga">18:00</p>
+                  </div>
+                  <div className="soat1">
+                    <p className="padruga">18:30</p>
+                  </div>
+                </div>
+              </div>
+              <div className="kalendar2">
+                <div className="k1">
+                  <h1>Мединцинский центр "Бегма"</h1>
+                  <p className="we">(Cupldltate voluptate)</p>
+                  <div className="kun">
+                    <div className="kun1">
+                      <p>6</p>
+                      <p>cp</p>
+                    </div>
+                    <div className="kun2">
+                      <p>7</p>
+                      <p>чт</p>
+                    </div>
+                    <div className="kun3">
+                      <p>8</p>
+                      <p>пт</p>
+                    </div>
+                    <div className="kun4">
+                      <p>9</p>
+                      <p>сб</p>
+                    </div>
+                    <div className="kun5">
+                      <z></z>
+                      <p>10</p>
+                      <p>вс</p>
+                    </div>
+                    <div className="kun6">
+                      <p>11</p>
+                      <p>пн</p>
+                    </div>
+                    <div className="kun7">
+                      <p>12</p>
+                      <p>вт</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="k2">
+                  <div className="soat1">
+                    <p className="padruga">16:00</p>
+                  </div>
+                  <div className="soat1">
+                    <p className="padruga">16:30</p>
+                  </div>
+                  <div className="soat1">
+                    <p className="padruga">17:00</p>
+                  </div>
+                  <div className="soat1">
+                    <p className="padruga">17:30</p>
+                  </div>
+                  <div className="soat1">
+                    <p className="padruga">18:00</p>
+                  </div>
+                  <div className="soat1">
+                    <p className="padruga">18:30</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        
       </div>
     </div>
   </div>
